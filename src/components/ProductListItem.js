b165ba0f5031649fb5520e6763cf74cd390dab43
geoblo <geoblo@naver.com>
2023-11-06 14:25:34 +0900

    프로젝트 업로드

---
+++ src/components/ProductListItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+
+// 방법1: 스타일드 컴포넌트로 스타일 확장
+const StyledCol = styled(Col)`
+  cursor: pointer;
+`;
+// 방법2: GlobalStyle에 공통 스타일로 작성
+
+function ProductListItem(props) {
+  // const { product: { title, price, imagePath } } = props;
+  const { product } = props;
+  const { id, title, price, imagePath } = product;
+
+  const navigate = useNavigate();
+
+  return (
+    <Col md={4} className='cursor-pointer'>
+      <img src={imagePath} width="80%"
+        // 상품 클릭 시 이동 경로 설정하기
+        onClick={() => {
+          // /detail/해당 상품 id
+          navigate(`/detail/${id}`);
+        }}
+      />
+      <h4>{title}</h4>
+      <p>{price}원</p>
+    </Col>
+  );
+}
+
+export default ProductListItem;
---
+++ src/pages/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+function Layout(props) {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {/* 헤더 영역: 상단 내비게이션 바 */}
+      <header>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#" onClick={() => navigate('/')}>고니네 샵</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => navigate('/')}>홈</Nav.Link>
+              <Nav.Link onClick={() => navigate('/cart')}>장바구니</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
+
+      <Outlet />
+
+      {/* <footer>푸터 영역</footer> */}
+    </>
+  );
+}
+
+export default Layout;
